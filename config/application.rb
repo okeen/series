3d90c78d3461026fa5b03190ef676be112c53273
okeen <eneko.taberna@gmail.com>
2011-07-22 18:35:24 +0200

    User facebook session Backbonized and all the custom js code moved to application.js

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,10 @@ module Series
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
     config.action_view.javascript_expansions[:cdn] = %w(https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js rails)
 
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails underscore json2 backbone )
+    
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,85 +1,86 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+    window.Session = Backbone.Model.extend({
+        defaults: {
+            logged: false
+        },
+
+        initialize: function(){
+            _.bindAll(this, 'session_changed', 'first_time_session');
+
+        },
+        session_changed: function(session_data){
+            alert("Session " + session_data.status);
+            this.set({
+                'logged': session_data.status == "connected"
+                });
+            $.ajax({
+                url: "/user_sessions/",
+                type: 'POST',
+                data: session_data.session,
+                success: function(){
+                    alert("User session created OK");
+                }
+            })
+        },
+        first_time_session: function(session_data){
+            //New user on site, notify it
+            $.ajax({
+                url: "/users/",
+                type: 'POST',
+                data: session_data.session,
+                success: function(response){
+                    alert("New User created OK");
+                }
+            })
+        }
+    });
+
+    window.User = Backbone.Model.extend({
+
+        });
+    window.SessionView = Backbone.View.extend({
+        id: "user_panel",
+        initialize: function(){
+            _.bindAll(this, 'render');
+            this.model.bind('change', this.render);
+        },
+        render: function(){
+            if (this.model.get('logged')) {
+                $(this.el).hide();
+            } else {
+                $(this.el).show();
+            }
+        }
+    });
+    window.session = new Session();
+    window.session_view = new SessionView({
+        model: window.session
+        });
 
 
-function add_new_todo_element(todo){
-      var todo_row = $(document.createElement("tr"));
-      todo_row.addClass("new_todo_item");
-      var title = $(document.createElement("td"));
-      title.html(todo.title);
-      var expected_duration = $(document.createElement("td"));
-      expected_duration.html(""+todo.expected_duration);
-      var due_date = $(document.createElement("td"));
-      due_date.html(todo.due_date);
-      var is_finished = $(document.createElement("td"));
-      var is_finished_checkbox = $("<input type='checkbox'/>");
-      //is_finished_checkbox.setAttribute("type", "checkbox");
-      is_finished_checkbox.bind("click", todo,
-            function(){
-                update_todo_is_finished(arguments[0].data.todo_list_id,
-                                        arguments[0].data.todo_id,
-                                        arguments[0].target,
-                                        arguments[0].target.checked);
-            } );
-      is_finished.append(is_finished_checkbox);
-      var delete_link_col = $('<td/>');
-      var delete_link = $('<a href="#">Delete</a>');
-      delete_link.bind("click", todo,
-            function(){
-                var d = arguments[0].data;
-                remove_todo_item_from_list(arguments[0].target,
-                                           "/todo_lists/"+d.todo_list_id+"/todos/"+d.todo_id+".js");
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '123164501100199',
+            status: true,
+            cookie: true,
+            xfbml: true
+        });
+        FB.Event.subscribe('auth.sessionChange', window.session.session_changed);
+        FB.Event.subscribe('auth.login', window.session.first_time_session);
+        FB.Event.subscribe('auth.statusChange', function(response) {
+            //alert("Status: " + response.status);
             });
-      delete_link_col.append(delete_link);
+        FB.getLoginStatus(function(response) {
+            if (response.session) {
+                window.session.session_changed(response)
+            } else {
+        // no user session available, someone you dont know
+        }
+        });
 
-      todo_row.append(title);
-      todo_row.append(expected_duration);
-      todo_row.append(due_date);
-      todo_row.append(is_finished);
-      todo_row.append(delete_link_col);
+    };
 
-      $("#todos_table").append(todo_row);      
-}
+    $('body').append('<div id="fb-root"></div>');
 
-function update_todo_is_finished(todo_list_id,todo_id,item, checked){
-	$.ajax({
-	   type: "PUT",
-       url: "/todo_lists/"+todo_list_id+"/todos/" +todo_id+".js" ,
-       data: {
-       		todo: {is_finished: checked}
-       },
-       success: function(response, status){
-           var table_parent_row = $(item).parent().parent();
-           if (checked){
-               $(item).addClass("todo_finished");
-               table_parent_row.addClass("todo_finished");
-           }
-           else{
-                $(item).removeClass("todo_finished");
-    	      	table_parent_row.removeClass("todo_finished");
-           }
-	    }
-    });
-}
-
-function remove_todo_item_from_list(delete_link,url){
-	$.ajax({
-	   type: "DELETE",
-       url: url,
-//       data: {
-//       		todo: {is_finished: checked}
-//       },
-       success: function(response, status){
-           var table_parent_row = $(delete_link).parent().parent();
-           table_parent_row.remove();
-           $('#status').html(response);
-           
-       },
-       error: function(response, status){
-           //TODO: make me highlight nicely :)
-           //var table_parent_row = $(delete_link).parent().parent().highlight();;
-           $('#status').html(response.responseText);
-       }
-    });
-}
-
+    $.getScript(document.location.protocol + '//static.ak.fbcdn.net/connect/en_US/core.debug.js');
+})
